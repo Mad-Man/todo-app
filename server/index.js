cca9495eb62166d8cb695fc2dd86cba8c10504dc
juan ignacio aldama <juan.aldama@mercadolibre.com>
2019-01-10 15:16:55 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const appPort = 3000;
+const notFoundTemplate = require('./templates/404');
+
+/*static files*/
+app.use('/public', express.static(path.join(__dirname, '..', 'app', 'public')));
+app.use('/public/bootstrap', express.static(path.join(__dirname, '..', 'node_modules', 'bootstrap', 'dist')));
+/*app endpoints*/
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '..', 'app', 'index.html')));
+app.get('/todo', (req, res, next) => res.sendFile(path.join(__dirname, '..', 'app', 'todo.html')));
+/*errors*/
+app.get('*', (req, res, next) => res.send(notFoundTemplate));
+
+app.listen(appPort, () => console.log(`listeing to port ${appPort}`))
+
---
+++ server/templates/404.js
@@ -0,0 +1,6 @@
+module.exports = `
+<div id="NotFoundContainer">
+ <p>¯\\_(ツ)_/¯</p>
+ <h1>404 - PAGE NOT FOUND</h1>
+ </div>
+`;
